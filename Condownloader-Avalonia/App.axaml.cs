2fafc620e90856d461fd8ca6945499f34f0339b3
Royce551 <poohwie2005@gmail.com>
2021-02-01 18:36:57 -0600

    maybe fix linux?

--- Condownloader-Avalonia/App.axaml.cs
+++ Condownloader-Avalonia/App.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Platform;
 
 namespace Condownloader_Avalonia
 {
@@ -13,12 +15,18 @@ namespace Condownloader_Avalonia
 
         public override void OnFrameworkInitializationCompleted()
         {
+            
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 desktop.MainWindow = new MainWindow();
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+        public override void RegisterServices()
+        {
+            AvaloniaLocator.CurrentMutable.Bind<IFontManagerImpl>().ToConstant(new CustomFontManagerImpl());
+            base.RegisterServices();
         }
     }
 }

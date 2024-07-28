a6770e6b21f39ab63cebe18b422bbcde2c69e421
Royce551 <poohwie2005@gmail.com>
2024-07-28 01:05:39 -0500

    Start on UI

--- Condownloader/ViewModels/MainWindowViewModel.cs
+++ Condownloader/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,38 @@
-﻿namespace Condownloader.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Condownloader.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-#pragma warning disable CA1822 // Mark members as static
-        public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+        [ObservableProperty]
+        private string downloadURL = string.Empty;
+
+        [ObservableProperty]
+        private string downloadFileName = string.Empty;
+
+        [ObservableProperty]
+        private bool downloadAudioOnly = false;
+
+        [ObservableProperty]
+        private int downloadVideoFormatIndex = 2;
+
+        [ObservableProperty]
+        private int downloadAudioFormatIndex = 1;
+
+        public void StartDownloadJob()
+        {
+
+        }
+
+        [ObservableProperty]
+        private string convertURL = string.Empty;
+
+        [ObservableProperty]
+        private string convertFormat = string.Empty;
+
+        public void StartConvertJob()
+        {
+
+        }
     }
 }

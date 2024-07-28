ae4fa02e0da5b2ac6e9d67cb8a3a39446b05fae6
Royce551 <poohwie2005@gmail.com>
2024-07-28 01:23:35 -0500

    port jobs from old project

---
+++ Condownloader/Jobs/IJob.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condownloader.Jobs
+{
+    public interface IJob
+    {
+        public EventHandler<JobErrorEventArgs> JobError { get; set; }
+        public EventHandler JobStateChanged { get; set; }
+        public LoggingManager Logs { get; set; }
+        public JobStatus Status { get; set; }
+        public string Name { get; set; }
+        void Start();
+        void Stop();
+    }
+}
---
+++ Condownloader/LoggingManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condownloader
+{
+    public class LoggingManager
+    {
+        public List<string> Logs { get; private set; } = new();
+        public EventHandler<string> NewLogsRecieved;
+        public void WriteLog(string log)
+        {
+            Logs.Add(log);
+            NewLogsRecieved?.Invoke(null, log);
+        }
+    }
+}

778daac8542160279865380ff6291717b5951f8d
Royce551 <poohwie2005@gmail.com>
2024-08-07 21:33:19 -0500

    Add about window

--- Condownloader/Views/MainWindow.axaml.cs
+++ Condownloader/Views/MainWindow.axaml.cs
@@ -8,5 +8,10 @@ namespace Condownloader.Views
         {
             InitializeComponent();
         }
+
+        private void NativeMenuItem_Click(object? sender, System.EventArgs e)
+        {
+            new AboutWindow().Show();
+        }
     }
 }
